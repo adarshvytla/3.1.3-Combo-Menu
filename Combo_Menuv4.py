b88675b8fde3505d003120499bd932b64671e828
adarshvytla <adarsh.vytla1234@gmail.com>
2026-05-29 21:49:16 -0700

    Add files via upload

---
+++ Combo_Menuv4.py
@@ -0,0 +1,60 @@
+cost = 0.0
+sandwich = input("What type of sandwhich would you like(chicken, beef, tofu)")
+if sandwich == "chicken":
+    cost += 5.25
+if sandwich == "beef" : 
+    cost += 6.25
+if sandwich == "tofu" :
+    cost += 5.75
+
+print("You have selected a", sandwich, "sandwhich, and it costs", cost, ".")
+
+beverage_choice = input("Would you like a beverage(yes or no)")
+beverage_size = "none"
+
+if beverage_choice == "yes" :
+    beverage_size = input("What size would you like your beverage(small, medium, large)")
+    if beverage_size == "small" :
+        cost += 1.00
+    elif beverage_size == "medium" :
+        cost += 1.75
+    elif beverage_size == "large" :
+        cost += 2.25
+    print("You have selected a", beverage_size, "beverage.")
+else:
+    print("You did not selected a beverage.")
+
+fries_choice = input("Would you like french fries(yes or no)?")
+fries_size = "none"
+
+if fries_choice == "yes" :
+    fries_size = input("What size would you like your fries(small, medium, large)")
+    if fries_size == "small" :
+        megasize = input("Would you like megasize fries(yes or no)")
+        if megasize == "yes" :
+            cost += 2.00
+            fries_size = "megasize"
+        else:
+            cost += 1.00
+    if fries_size == "medium" :
+        cost += 1.50
+    if fries_size == "large" :
+        cost += 2.00
+
+ketchup = int(input("How many ketchup packets would you like? "))
+cost += (ketchup * 0.25)
+
+if sandwich != "none" and beverage_choice == "yes" and fries_choice == "yes":
+    cost -= 1.00
+
+print("Order Summary")
+if sandwich != "none":
+    print("Sandwich:", sandwich)
+if beverage_choice == "yes":
+    print("Beverage:", beverage_size)
+if fries_choice == "yes":
+    print("Fries:", fries_size)
+if ketchup > 0:
+    print("Ketchup packets:", ketchup)
+
+print("Total Cost:", cost)
